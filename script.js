16883be3e7fe25fb1d1500d6ed8f942c4cf49444
Pawiu__ <pawelkoc681@gmail.com>
2022-11-15 09:33:00 +0100

    dads
    dsadsa

---
+++ script.js
@@ -0,0 +1,18 @@
+// var dane = document.getElementsByClassName('input')
+var dane1 = document.getElementById('dane1').value
+var dane2 = document.getElementById('dane2').value
+var wynik;
+
+document.getElementById('oblicz').addEventListener('click', ()=>{
+    switch (dane1){
+        case 1:
+            wynik = dane2 * 4
+            break;
+        case 2:
+            wynik = dane2 * 3,5
+            break;
+        default:
+            wynik = dane2 * 0 
+    }
+    document.getElementById('wynik').innerHTML = `koszt paliwa: ${wynik}zł`
+})
